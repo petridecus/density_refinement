34d4e600433eda7114ac01c5c60431ee631a5269
petridecus <apetrides32@gmail.com>
2022-12-09 13:11:54 -0500

    deleted unnecessary files

--- realspace_correl.m
+++
@@ -1,5 +0,0 @@
-function [rscc]=realspace_correl(rho_1,rho_2,mask)
-%
-
-c = corrcoef( rho_1(mask),rho_2(mask) );
-rscc = c(1,2);
--- test_plotter.m
+++
@@ -1,7 +0,0 @@
-coords = [[2.0, 1.1]; ...
-          [4.2, 3.7]; ...
-          [2.2, 1.3]];
-
-[rho, mask] = rhoc(coords, [ 5 , 5 ], .01, rand());
-
-imagesc(rho);
